c9e10644806e553b2418ea0a571824d5be851546
caffe <caffe@caffenoMacBook-Pro.local>
2018-12-08 22:43:17 +0900

    デザイン見直し、モーダル仕上げ

--- src/js/script.js
+++ src/js/script.js
@@ -1,31 +1,34 @@
 var HSHP = HSHP || {};
 
-HSHP.MODAL_MANAGER = {
+HSHP.MODAL_MANAGER = function ($modalGroup) {
+  this.$modalGroup = $modalGroup;
+  this.init();
+};
+
+HSHP.MODAL_MANAGER.prototype = {
+  TIMER: 250,
   init: function () {
     this.setParameter();
     this.bindEvent();
   },
   setParameter: function () {
+    this.$modalItem = this.$modalGroup.find('.jscModalItem');
     this.$modalWrap = $('.jscModal');
     this.$closeTrigger = $('.jscModalClose');
     this.$next = $('.jscModalNext');
     this.$prev = $('.jscModalPrev');
-    this.$modalItem = $('.jscModalItem');
 
     this.$modalImage = $('.jscModalImage');
     this.$modalTitle = $('.jscModalTitle');
     this.$modalDate = $('.jscModalDate');
     this.$modalDescription = $('.jscModalDescription');
+    this.$currentNum = $('.jscCurrentNum');
+    this.$itemLength = $('.jscItemLength');
+    this.$modalInfoContents = $('.jscModalMainContents');
   },
   bindEvent: function () {
     this.$closeTrigger.on('click', $.proxy(this.closeModal, this));
     this.$modalItem.on('click', $.proxy(this.openModal, this));
-    this.$next.on('click', $.proxy(function () {
-      this.setItem('next');
-    }, this));
-    this.$prev.on('click', $.proxy(function () {
-      this.setItem('prev');
-    }, this));
   },
   closeModal: function () {
     this.$modalWrap.css({
@@ -34,11 +37,16 @@ HSHP.MODAL_MANAGER = {
   },
   openModal: function (event) {
     this.$currentItem = $(event.currentTarget);
-
     this.$modalWrap.css({
       'display': 'block'
     });
     this.setData();
+    // this.$next[0].addEventListener('click', this.setItem.bind(this, 'next'), false);
+    // this.$prev[0].addEventListener('click', this.setItem.bind(this, 'prev'), false);
+    this.$next.off('click', $.proxy(this.setItem, this, 'next'));
+    this.$prev.off('click', $.proxy(this.setItem, this, 'prev'));
+    this.$next.on('click', $.proxy(this.setItem, this, 'next'));
+    this.$prev.on('click', $.proxy(this.setItem, this, 'prev'));
   },
   setData: function () {
     var data = {
@@ -51,20 +59,57 @@ HSHP.MODAL_MANAGER = {
     this.$modalImage.css({
       'background-image': 'url("' + data.image.attr('src') + '")'
     });
+    this.$currentNum.text(this.$currentItem.index() + 1);
+    this.$itemLength.text(this.$modalItem.length);
     this.$modalTitle.text(data.title);
     this.$modalDate.text(data.date);
     this.$modalDescription.text(data.description);
   },
   setItem: function (trigger) {
-    if (trigger === 'next') {
-      this.$currentItem = this.$currentItem.next().length ? this.$currentItem.next() : $('.jscModalItem:first-child');
-    } else if (trigger === 'prev') {
-      this.$currentItem = this.$currentItem.prev().length ? this.$currentItem.prev() : $('.jscModalItem:last-child');
+    if (this.$modalInfoContents.is(':animated')) {
+      return;
     }
-    this.setData();
+
+    this.$modalInfoContents.fadeOut(this.TIMER, function () {
+      if (trigger === 'next') {
+        this.$currentItem = this.$currentItem.next().length ? this.$currentItem.next() : this.$modalGroup.find('.jscModalItem:first-child');
+      } else if (trigger === 'prev') {
+        this.$currentItem = this.$currentItem.prev().length ? this.$currentItem.prev() : this.$modalGroup.find('.jscModalItem:last-child');
+      }
+      this.setData();
+    }.bind(this));
+    setTimeout(function () {
+      this.$modalInfoContents.fadeIn(this.TIMER);
+    }.bind(this), this.TIMER);
+  }
+}
+
+HSHP.KV_SCROLL_INTERACTION = {
+  init: function () {
+    this.setParameter();
+    this.scrollEvent();
+  },
+  setParameter: function () {
+    this.$window = $(window);
+    this.$kv = $('.jscKV');
+  },
+  scrollEvent: function () {
+    var self = this;
+    var kvHeight = this.$kv.outerHeight();
+    this.$window.on('scroll', function () {
+      var scrollTop = $(this).scrollTop();
+      if (kvHeight >= scrollTop) {
+        self.$kv.css({
+          'background-position': '50% calc(50% + ' + scrollTop + 'px)'
+        });
+      }
+    });
   }
 }
 
 $(function () {
-  HSHP.MODAL_MANAGER.init();
+  $('.jscModalGroup').each(function () {
+    new HSHP.MODAL_MANAGER($(this));
+  });
+  HSHP.KV_SCROLL_INTERACTION.init();
 });
